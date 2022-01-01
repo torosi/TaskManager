6562f367a95c42a9fac6a94de70099a40fa2b40c
Thomas simons <thomassimons98@gmail.com>
2022-01-01 14:07:54 +0000

    Add project files.

---
+++ TaskManager/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models.ViewModel
+{
+    public class LoginViewModel
+    {
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+
+
+    }
+}
